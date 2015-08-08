f2073f8183aefdc55675e2c25b17df0e3df87616
beolnix <beolnix@gmail.com>
2015-08-08 14:31:54 +0300

    unit tests for CloudImageStorage

---
+++ tcdop-server/src/test/groovy/io/cyberstock/tcdop/server/integration/digitalocean/storage/CloudImageStorageTestCase.groovy
@@ -0,0 +1,119 @@
+package io.cyberstock.tcdop.server.integration.digitalocean.storage
+
+import com.myjeeva.digitalocean.pojo.Image
+import io.cyberstock.tcdop.model.DOSettings
+import io.cyberstock.tcdop.model.error.DOError
+import io.cyberstock.tcdop.server.integration.digitalocean.DOClientService
+import io.cyberstock.tcdop.server.integration.digitalocean.DOClientServiceFactory
+import io.cyberstock.tcdop.server.integration.digitalocean.storage.impl.CloudImageStorageFactoryImpl
+import io.cyberstock.tcdop.server.integration.teamcity.DOCloudImage
+import io.cyberstock.tcdop.server.integration.teamcity.DOCloudInstance
+import org.testng.annotations.Test
+
+import java.util.concurrent.Executors
+
+import static org.testng.Assert.assertEquals
+import static org.testng.Assert.assertNotNull
+import static org.testng.Assert.assertTrue
+
+/**
+ * Created by beolnix on 02/08/15.
+ */
+class CloudImageStorageTestCase {
+
+    private final static Integer IMAGE_ID = Random.newInstance().nextInt();
+
+    @Test
+    public void testGetById() {
+        def executorService = Executors.newFixedThreadPool(1)
+        def factory = new CloudImageStorageFactoryImpl(new DOClientServiceFactoryTestImpl())
+        def storage = factory.getStorage(executorService, "test")
+
+        delay();
+
+        assertNotNull(storage.getImageById(IMAGE_ID.toString()))
+    }
+
+
+    @Test
+    public void getImagesTest() {
+        def executorService = Executors.newFixedThreadPool(1)
+        def factory = new CloudImageStorageFactoryImpl(new DOClientServiceFactoryTestImpl())
+        def storage = factory.getStorage(executorService, "test")
+
+        delay();
+
+        def map = storage.getImagesList()
+
+        assertNotNull(map)
+        assertTrue(map.size() > 0)
+    }
+
+    @Test
+    public void instancesCountTest() {
+        def executorService = Executors.newFixedThreadPool(1)
+        def factory = new CloudImageStorageFactoryImpl(new DOClientServiceFactoryTestImpl())
+        def storage = factory.getStorage(executorService, "test")
+
+        delay();
+
+        def count = storage.getInstancesCount()
+
+        assertEquals(count, 1)
+    }
+
+    public void delay() {
+        try {
+            sleep(500)
+        } catch (InterruptedException e) {
+            e.printStackTrace()
+        }
+    }
+
+    public static class DOClientServiceFactoryTestImpl implements DOClientServiceFactory {
+        @Override
+        DOClientService createClient(String token) {
+            return new DOClientService() {
+                @Override
+                List<DOCloudImage> getImages() {
+                    def doImage = new Image(id: IMAGE_ID, name: "test")
+                    def image = new DOCloudImage(doImage)
+
+                    image.addInstance(new DOCloudInstance(image, "123", "test"))
+
+                    return [image]
+                }
+
+                @Override
+                void waitInstanceInitialization(DOCloudInstance cloudInstance) {
+
+                }
+
+                @Override
+                void restartInstance(DOCloudInstance cloudInstance) {
+
+                }
+
+                @Override
+                void terminateInstance(DOCloudInstance cloudInstance) {
+
+                }
+
+                @Override
+                DOCloudInstance createInstance(DOCloudImage cloudImage, DOSettings doSettings) throws DOError {
+                    return null
+                }
+
+                @Override
+                void accountCheck() throws DOError {
+
+                }
+
+                @Override
+                DOCloudImage findImageByName(String imageName) throws DOError {
+                    return null
+                }
+            }
+        }
+    }
+}
