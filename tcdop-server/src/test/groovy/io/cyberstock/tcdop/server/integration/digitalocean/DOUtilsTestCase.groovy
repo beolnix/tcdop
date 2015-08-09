39216737152175f18359248b1dd9f3be49ce4965
beolnix <beolnix@gmail.com>
2015-08-09 20:06:48 +0300

    - adding tests for DOUtils

--- tcdop-server/src/test/groovy/io/cyberstock/tcdop/server/integration/digitalocean/DOUtilsTestCase.groovy
+++ tcdop-server/src/test/groovy/io/cyberstock/tcdop/server/integration/digitalocean/DOUtilsTestCase.groovy
@@ -2,6 +2,7 @@ package io.cyberstock.tcdop.server.integration.digitalocean
 
 import com.myjeeva.digitalocean.DigitalOcean
 import com.myjeeva.digitalocean.impl.DigitalOceanClient
+import com.myjeeva.digitalocean.pojo.Account
 import com.myjeeva.digitalocean.pojo.Droplet
 import com.myjeeva.digitalocean.pojo.Image
 import com.myjeeva.digitalocean.pojo.Region
@@ -12,7 +13,10 @@ import io.cyberstock.tcdop.server.integration.teamcity.DOCloudImage
 import io.cyberstock.tcdop.server.integration.teamcity.web.DOSettingsUtils
 import org.testng.annotations.Test
 
-import static org.testng.Assert.assertNotNull;
+import static org.testng.Assert.assertEquals
+import static org.testng.Assert.assertEqualsNoOrder
+import static org.testng.Assert.assertNotNull
+import static org.testng.Assert.assertTrue;
 
 /**
  * Created by beolnix on 08/08/15.
@@ -20,26 +24,48 @@ import static org.testng.Assert.assertNotNull;
 class DOUtilsTestCase {
 
     def client = [createDroplet: { droplet ->
-        return new Droplet()
+        return droplet
     }] as DigitalOcean
+
+    def PREFIX = "prefix"
+
 
     @Test
     public void createInstanceTest() {
-
-
         DOSettings settings = DOSettingsUtils.convertToDOSettings(getParametersMap())
+        def IMAGE_ID = 123
+        def REGION_SLUG = "test"
+
         DOCloudImage cloudImage = new DOCloudImage(
-                new Image(id: 123,
+                new Image(id: IMAGE_ID,
                         name: "test",
                         minDiskSize: settings.getSize().diskSize,
                         slug: "deb",
-                        regions: ["test"]
+                        regions: [REGION_SLUG]
                         )
                 )
 
 
         Droplet droplet = DOUtils.createInstance(client, settings, cloudImage)
+
         assertNotNull(droplet)
+        assertTrue(droplet.name.startsWith(PREFIX))
+        assertEquals(droplet.size, settings.getSize().slug)
+        assertEquals(droplet.image.id, IMAGE_ID)
+        assertEquals(droplet.region.slug, REGION_SLUG)
+    }
+
+    @Test
+    public void getAccountTest() {
+        def getAccountInfoExecuted = false
+        def client = [getAccountInfo: {
+            getAccountInfoExecuted = true
+            return new Account()
+        }] as DigitalOcean
+
+        DOUtils.checkAccount(client)
+
+        assertTrue(getAccountInfoExecuted)
     }
 
     def getParametersMap() {
@@ -49,7 +75,7 @@ class DOUtilsTestCase {
         params[WebConstants.TOKEN] = 'test'
         params[WebConstants.DROPLET_SIZE] = '32GB'
         params[WebConstants.IMAGE_NAME] = 'name'
-        params[WebConstants.DROPLET_NAME_PREFIX] = 'prefix'
+        params[WebConstants.DROPLET_NAME_PREFIX] = PREFIX
         params
     }
 }
