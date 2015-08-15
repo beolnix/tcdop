65476e84534297fe9c239dd64a0cf3199fb67c13
beolnix <beolnix@gmail.com>
2015-08-15 23:38:00 +0300

    adding tests for SOClientService: createInstance

--- tcdop-server/src/test/groovy/io/cyberstock/tcdop/server/integration/digitalocean/DOClientServiceTestCase.groovy
+++ tcdop-server/src/test/groovy/io/cyberstock/tcdop/server/integration/digitalocean/DOClientServiceTestCase.groovy
@@ -1,11 +1,15 @@
 package io.cyberstock.tcdop.server.integration.digitalocean
 
+import com.myjeeva.digitalocean.pojo.Droplet
 import com.myjeeva.digitalocean.pojo.Image
+import io.cyberstock.tcdop.model.DOConfigConstants
+import io.cyberstock.tcdop.model.WebConstants
 import io.cyberstock.tcdop.server.integration.digitalocean.adapter.DOAdapter
 import io.cyberstock.tcdop.server.integration.digitalocean.adapter.impl.DOAdapterImpl
 import io.cyberstock.tcdop.server.integration.digitalocean.impl.DOClientServiceImpl
 import io.cyberstock.tcdop.server.integration.teamcity.DOCloudImage
 import io.cyberstock.tcdop.server.integration.teamcity.DOCloudInstance
+import io.cyberstock.tcdop.server.integration.teamcity.web.DOSettingsUtils
 import org.testng.annotations.Test
 
 import static org.testng.Assert.assertTrue
@@ -83,7 +87,36 @@ class DOClientServiceTestCase {
 
         assertTrue(match)
         assertTrue(instance.image.instances.size() == 0)
+    }
 
+    @Test
+    public void createInstance() {
+        def originalImage = new DOCloudImage(new Image(id: 123))
+        def settings = DOSettingsUtils.convertToDOSettings(getParametersMap())
+        def droplet = new Droplet(id: 123, name: "test")
+
+        def adapter = [
+                createInstance: { doSettings, image ->
+                    return droplet
+                }] as DOAdapter
+
+
+        DOClientServiceImpl service = new DOClientServiceImpl(adapter);
+        DOCloudInstance instance = service.createInstance(originalImage, settings)
+
+        assertEquals(instance.name, droplet.name)
+        assertTrue(instance.image.instances.size() == 1)
+    }
+
+    def getParametersMap() {
+        def params = [:]
+        params[WebConstants.DO_INTEGRATION_MODE] = DOConfigConstants.PREPARED_IMAGE_MODE_CODE
+        params[WebConstants.INSTANCES_COUNT_LIMIT] = '5'
+        params[WebConstants.TOKEN] = 'test'
+        params[WebConstants.DROPLET_SIZE] = '32GB'
+        params[WebConstants.IMAGE_NAME] = 'name'
+        params[WebConstants.DROPLET_NAME_PREFIX] = 'prefix'
+        params
     }
 
 }
